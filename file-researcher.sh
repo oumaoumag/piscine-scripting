3daa6fd8aaf591de9cee6285c0930097a65ae3f1
oumaoumag <ouma.godwin10@gmail.com>
2025-11-03 22:57:15 +0300

    update flags

--- file-researcher.sh
+++ file-researcher.sh
@@ -4,4 +4,4 @@
 grep '^It takes 12 honey' file-researcher/facts.txt | sed 's/^[0-9]*://'
 
 # Find all lines that contain the word "year"
-grep year file-researcher/facts.txt | sed 's/^[0-9]*://'
+grep year file-researcher/facts.txt
