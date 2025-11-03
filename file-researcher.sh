4fac3bd880d8d1078f498f681203bdeb9cae074e
oumaoumag <ouma.godwin10@gmail.com>
2025-11-03 21:51:27 +0300

    update grep flags

--- file-researcher.sh
+++ file-researcher.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Find the line that starts with "It takes 12 honey."
-grep "^It takes 12 honey" file-researcher/facts.txt
+grep '^It takes 12 honey' file-researcher/facts.txt
 
 # Find all lines that contain the word "year"
-grep -i "year" file-researcher/facts.txt | cut -d: -f2-
+grep -i year file-researcher/facts.txt | cut -d: -f2-
+
+
+
