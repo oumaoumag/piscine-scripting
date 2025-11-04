500f7eb5e4b302d1bb61494a58d260d795a76648
oumouma <ouma.godwin10@gmail.com>
2025-11-04 10:47:52 +0300

    add grep -n to script

--- file-researcher.sh
+++ file-researcher.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 FILE="file-researcher/facts.txt"
 
 # Find the line that starts with "It takes 12 honey."
 grep '^It takes 12 honey' "$FILE"
 
 # Find all lines that contain the word "year"
-grep 'year' "$FILE"
+grep -n 'year' "$FILE"
