b55c89a470d86d47ef2f61736e6b1f97dbfc115b
oumouma <ouma.godwin10@gmail.com>
2025-11-05 15:10:06 +0300

    create a script to read folders

---
+++ find-files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ZIP_URL="https://assets.01-edu.org/devops-branch/find-files-example.zip"
+
+ZIP_FILE=$(basename "$ZIP_URL")
+EXTRACT_DIR="extracted_data"
+
+curl -sL -O "$ZIP_URL"
+
+echo "Extracting.."
+mkdir -p "$EXTRACT_DIR"
+unzip -q "$ZIP_FILE" -d "$EXTRACT_DIR"
+
+echo "Directories found:"
+find "$EXTRACT_DIR" -type d
+
+# 4. Clean up the downloaded zip file
+rm "$ZIP_FILE"
--- head-and-tail.sh
+++ head-and-tail.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-response=$(curl -s https://learn.zone01kisumu.ke/assets/devops-branch/HeadTail.txt) 
+response=$(curl -s https://learn.zone01kisumu.ke/assets/devops-branch/HeadTail.txt)
 
-head -n 1 <<< "$response"
-
-tail -n 1 <<< "$response" 
+head -n 1 <<< $response
+tail -n 1 <<< $response
