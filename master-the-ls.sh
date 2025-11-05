20a12fc16f686f309c3f5168c2eec23f04865b0b
oumaoumag <ouma.godwin10@gmail.com>
2025-11-05 23:48:37 +0300

    add script

--- master-the-ls.sh
+++ master-the-ls.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-# Use find for full control — this is the PROFESSIONAL way
-
-find . -maxdepth 1 ! -name '.' -type d -exec printf '%s/\n' {} + \
-       -o ! -name '.' -type f -printf '%s\n' {} + 2>/dev/null | 
-  grep -v '/\.' | 
-  sort -t '\0' -k 1.3 | 
-  awk '{printf "%s%s", $0, (NR>1?",":"")} END {if(NR>0) print ""}'
+ls -1Autp | paste -sd, - | sed 's/,$//'
