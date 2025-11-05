d8637790beddec0447956098d5343cb6bd05b88b
oumouma <ouma.godwin10@gmail.com>
2025-11-05 18:19:24 +0300

    add master-the-ls

--- master-the-ls.sh
+++ master-the-ls.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ls -1At | tail -n +2 | while IFS= read -r f; do
+ls -1At | tail -n +2 | grep -v '^\.' | while IFS= read -r f; do
     [ -d "$f" ] && printf "%s/," "$f" || printf "%s," "$f"
 done | sed 's/,$/\n/'
