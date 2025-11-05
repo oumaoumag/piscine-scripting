afc30535506f6262ec4541a5f0bb02432c3ac954
oumouma <ouma.godwin10@gmail.com>
2025-11-05 15:15:56 +0300

    remove  echo

--- find-files.sh
+++ find-files.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-ZIP_URL="https://assets.01-edu.org/devops-branch/find-files-example.zip"
-
-ZIP_FILE=$(basename "$ZIP_URL")
-EXTRACT_DIR="extracted_data"
-
-curl -sL -O "$ZIP_URL"
-
-mkdir -p "$EXTRACT_DIR"
-unzip -q "$ZIP_FILE" -d "$EXTRACT_DIR"
-
-find "$EXTRACT_DIR" -type d
+DIR="${1:-.}"
+find "$DIR" -name "a*" -o -name "*z"
