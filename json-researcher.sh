0d8567b84d985e5c0cb37f09b576a0d2305c0ea3
oumouma <ouma.godwin10@gmail.com>
2025-11-05 13:49:32 +0300

    add script to search through a json file and extract data without saving it in file

--- json-researcher.sh
+++ json-researcher.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json \
-| jq -r ".[] | select(.id == 1) | .name, .powerstats.power" \
-| grep -E '^[A-Za-z0-9\ -]+$' \
-| awk '{print "\"", "\","}'
+| jq  ".[] | select(.id == 1) \
+| grep -E '"name" | "power"'
