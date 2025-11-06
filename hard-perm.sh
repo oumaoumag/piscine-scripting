2fc3e9ec00be560550cec95a9e2ffdb9810877d5
oumaoumag <ouma.godwin10@gmail.com>
2025-11-06 07:25:43 +0300

    update permission in files

--- hard-perm.sh
+++ hard-perm.sh
@@ -4,10 +4,10 @@ chmod 401 hard-perm/0
 chmod 402 hard-perm/1
 chmod 604 hard-perm/2
 chmod 777 hard-perm/3
-chmod 511 hard-perm/4
+chmod 510 hard-perm/4
 chmod 460 hard-perm/5
 chmod 460 hard-perm/6
-chmod 511 hard-perm/7
+chmod 510 hard-perm/7
 chmod 604 hard-perm/8
 chmod 402 hard-perm/9
 chmod 401 hard-perm/A
