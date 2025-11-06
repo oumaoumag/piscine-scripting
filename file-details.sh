b92f927a9507ba5783b4e74ca97703fa7a5813b8
oumaoumag <ouma.godwin10@gmail.com>
2025-11-06 07:42:40 +0300

    add file structe:

--- file-details.sh
+++ file-details.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ls -l --time-style=+%F\ %R hard-perm | sed '1d' | awk '{print $1, $6, $7, $8}'
+# Navigate to the folder relative to this script
+cd "$(dirname "$0")/hard-perm" || { echo "hard-perm directory not found"; exit 1; }
+
+# List details: permissions, date, time, filename
+ls -l --time-style=+%F\ %R | sed '1d' | awk '{print $1, $6, $7, $8}'
--- files-details.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ls -l --time-style=+%F\ %R hard-perm | sed '1d' | awk '{print $1, $6, $7, $8}'
