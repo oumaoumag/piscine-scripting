49af2dba89fe18c13889a973c04d5cb807c1ddd2
oumaoumag <ouma.godwin10@gmail.com>
2025-11-06 07:49:28 +0300

    add file structe:

--- file-details.sh
+++ file-details.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-DIR="${1:-hard-perm}"
-[[ -d "$DIR" ]] || exit 1
-ls -l --time-style=+%F\ %H:%M "$DIR" 2>/dev/null |
-  tail -n +2 |
-  awk '{printf "%s %s %s %s\n", $1, $6, $7, $9}'
+
+ls -l hard-perm --time-style=+%F\ %R | sed '1d' | awk '{print $1, $6, $7, $NF}'
