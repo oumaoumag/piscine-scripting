066425d0b02c94458ef4182b0b520f83363c4d52
oumaoumag <ouma.godwin10@gmail.com>
2025-11-06 07:48:02 +0300

    add file structe:

--- file-details.sh
+++ file-details.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-DIR="${1:-.}"
-
-[[ -d "$DIR" ]] || { echo "Error: '$DIR' not found" >&2; exit 1; }
-
+DIR="${1:-hard-perm}"
+[[ -d "$DIR" ]] || exit 1
 ls -l --time-style=+%F\ %H:%M "$DIR" 2>/dev/null |
   tail -n +2 |
   awk '{printf "%s %s %s %s\n", $1, $6, $7, $9}'
