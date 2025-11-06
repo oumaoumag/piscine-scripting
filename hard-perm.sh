e7e63d48fef3e24b6a44fc1b9b05c909db56696b
oumaoumag <ouma.godwin10@gmail.com>
2025-11-06 07:20:34 +0300

    update permission in files

--- hard-perm.sh
+++ hard-perm.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# hard-perm.sh – Corrected version for Level 5
 
 cd hard-perm || { echo "Folder not found"; exit 1; }
 
-# Directories
 chmod 501 0 A
 chmod 777 3
 
-# Files
-chmod 604 1 9
+chmod 602 1 9
 chmod 640 2 5 6 8
-chmod 550 4 7
+chmod 510 4 7
