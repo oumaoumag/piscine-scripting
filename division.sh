81d5ebfab72bcd36efe855a76927c52b5b42812b
oumaoumag <ouma.godwin10@gmail.com>
2025-11-07 22:23:22 +0300

    add division

--- division.sh
+++ division.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 2 ]]; then
-    echo "Error: two numbers must be provided"
-    exit 1
-fi
-
-if [[ ! $1 =~ ^-?[0-9]+$ ]] || [[ ! $2 =~ ^-?[0-9]+$ ]]; then
-    echo "Error: both arguments must be integers"
-    exit 1
-fi
-
-if [[ $2 -eq 0 ]]; then
-    echo "Error: division by zero is not allowed"
-    exit 1
-fi
-
+[[ $# -ne 2 ]] && echo "Error: two numbers must be provided" && exit
+[[ ! $1 =~ ^-?[0-9]+$ ]] || [[ ! $2 =~ ^-?[0-9]+$ ]] && echo "Error: both arguments must be integers" && exit
+[[ $2 -eq 0 ]] && echo "Error: division by zero is not allowed" && exit
 echo "$1 / $2" | bc
