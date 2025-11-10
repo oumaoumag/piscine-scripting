dd4670a676a1e0701a34b1ea29ab8fc124a1232d
oumaoumag <ouma.godwin10@gmail.com>
2025-11-10 10:56:49 +0300

    functions

---
+++ numerical_operations.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def power(a, b):
+    return a ** b
+
+def square_root(a):
+    return a ** 0.5
