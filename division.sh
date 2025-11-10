2d2cf17bb2ef338763c0005cf09bf3d5baaf6ff1
oumaoumag <ouma.godwin10@gmail.com>
2025-11-10 10:21:03 +0300

    fix division

--- division.sh
+++ division.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 [[ $# -ne 2 ]] && echo "Error: two numbers must be provided" && exit
-(( $1 )) 2>/dev/null || [[ $1 == 0 ]] || { echo "Error: both arguments must be integers" && exit; }
-(( $2 )) 2>/dev/null || [[ $2 == 0 ]] || { echo "Error: both arguments must be integers" && exit; }
+[[ $1 =~ ^-?[0-9]+$ ]] || { echo "Error: both arguments must be integers" && exit; }
+[[ $2 =~ ^-?[0-9]+$ ]] || { echo "Error: both arguments must be integers" && exit; }
 [[ $2 -eq 0 ]] && echo "Error: division by zero is not allowed" && exit
 echo "$1 / $2" | bc
