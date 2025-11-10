2a88f6cb4c0c705e9b5639f274d1ecfc4ae0d514
oumaoumag <ouma.godwin10@gmail.com>
2025-11-10 11:26:17 +0300

    clean the list

---
+++ clean_the_list.py
@@ -0,0 +1,6 @@
+def clean_list(shopping_list):
+    if not shopping_list:
+        return []
+    if 'milk' not in [item.strip().lower() for item in shopping_list]:
+        shopping_list.append('milk')
+    return [f"{i+1}/ {item.strip().capitalize()}" for i, item in enumerate(shopping_list)]
